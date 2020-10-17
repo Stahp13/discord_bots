b3fdacbd23afb42bccb3f973c174dc94fa5f1547
codpcleague <69830207+codpcleague@users.noreply.github.com>
2020-10-16 23:28:10 -0400

    Update sq_show.py
    updated help definitions further

--- qbot_lib/sq_show.py
+++ qbot_lib/sq_show.py
@@ -25,8 +25,7 @@ class sq_show:
         await self.bot.temporary_message(message.channel, content = content, embed=embed)
     def help(self) -> discord.Embed:
         embed = discord.Embed(title='help', color=discord.Colour.orange())
-        content = """example: "sq!show"
-        This command is used to display the queue.
+        content = """This command is used to display the queue.
         """
         embed.add_field(name=f'syntax: sq!show', value=content, inline=False)
         return embed
